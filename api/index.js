5438ba957e80b4b179ec68614c559e4a63d13a4e
Soham Patil <sohampatilsp55@gmail.com>
2024-12-22 19:35:50 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -4,8 +4,20 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!!!');
+    }).catch((err) => {
+        console.log(err);
+}); 
+
 
 
 app.listen(3000, () => {
     console.log("Server is running at 3000 !!");
 });
+
+
