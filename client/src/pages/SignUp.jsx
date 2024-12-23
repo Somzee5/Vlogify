db45d392329536954289e2d07743e6dd53ef5a6b
Soham Patil <sohampatilsp55@gmail.com>
2024-12-23 21:01:13 +0530

    Applied all the functionality to signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const TypingText = () => {
   const phrases = [
@@ -35,9 +36,59 @@ const TypingText = () => {
 
 export default function SignUp() {
 
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const navigate = useNavigate();
+
+
   const handleChange = (e) => {
-     
-  }
+     setFormData({
+      ...formData,
+      [e.target.id] : e.target.value,
+     });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try
+    {
+      setLoading(true);
+
+      const res = await fetch('/api/auth/sign-up',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        },
+      );
+
+      const data = await res.json();
+      console.log(data);
+
+      if(data.success === false)
+      {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    }
+    catch(error)
+    {
+      setLoading(false);
+      setError(error.message);
+    }
+
+    
+  };
+
+  console.log(formData);
 
   return (
     <div className="flex h-screen bg-gradient-to-b from-gray-900 via-black to-gray-800">
@@ -51,39 +102,40 @@ export default function SignUp() {
 
       {/* Right Section */}
       <div className="flex justify-center items-center w-1/2 bg-gray-900 p-10">
-        <div className="w-full max-w-sm space-y-6">
+        <div className="w-full max-w-sm flex flex-col space-y-6">
           <h2 className="text-center text-2xl font-semibold text-gray-100">
             Create your account
           </h2>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit} className="space-y-4">
             <input
               type="text"
               id="username"
               placeholder="Username"
               className="mt-1 block w-full rounded-md bg-gray-800 border border-gray-700 py-2 px-3 text-white placeholder-gray-500 focus:ring-indigo-500 focus:outline-none"
-              onChange={ handleChange }
+              onChange={handleChange}
             />
             <input
               type="email"
               id="email"
               placeholder="Enter email address"
               className="mt-1 block w-full rounded-md bg-gray-800 border border-gray-700 py-2 px-3 text-white placeholder-gray-500 focus:ring-indigo-500 focus:outline-none"
-              onChange={ handleChange }
+              onChange={handleChange}
             />
             <input
               type="password"
               id="password"
               placeholder="Enter password"
               className="mt-1 block w-full rounded-md bg-gray-800 border border-gray-700 py-2 px-3 text-white placeholder-gray-500 focus:ring-indigo-500 focus:outline-none"
-              onChange={ handleChange }
+              onChange={handleChange}
             />
 
             <button
               type="submit"
+              disabled={loading}
               className="w-full rounded-md bg-indigo-500 py-2 px-4 text-sm font-bold text-white shadow-lg hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             >
-              Signup
+              {loading ? 'Loading...' : 'SignUp'}
             </button>
 
             <p className="mt-4 text-center text-sm text-gray-400">
@@ -93,8 +145,15 @@ export default function SignUp() {
               </a>
             </p>
           </form>
+
+          {error && (
+            <div className="mt-4 text-center">
+              <p className="text-red-600">{error}</p>
+            </div>
+          )}
         </div>
       </div>
+
     </div>
   );
 }
